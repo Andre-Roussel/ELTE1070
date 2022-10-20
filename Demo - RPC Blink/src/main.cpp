a0ce76e1a502ea78487d6c686e9138d4da9a8cc3
Andre Roussel <andre.roussel@ccnb.ca>
2022-10-20 12:51:22 -0300

    General Comment

--- Demo - RPC Blink/src/main.cpp
+++ Demo - RPC Blink/src/main.cpp
@@ -1,3 +1,14 @@
+/*
+  Titre      : DEMO - RPC Blink
+  Auteur     : André Roussel
+  Date       : 20/10/2022
+  Description: Démonstration de l'abonement a un message MQTT (RPC) et son affichage dans le moniteur série
+  Droits     : Reproduction permise pour usage pédagogique
+  Version    : 0.0.1
+*/
+
+
+
 #include <Arduino.h>
 #include "WIFI_NINA_Connector.h"
 #include "MQTTConnector.h"
